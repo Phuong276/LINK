9db0f9a18a93dadebf9aa334f383117e401a37cf
Dipro <dipro@DPMACBOOK31.lan>
2024-03-29 14:58:54 +0700

    test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,29 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('apple-app-site-association')
+  getAppleApp(res: Response) {
+    const body = {
+      applinks: {
+        apps: [],
+        details: [
+          {
+            appID: 'Y7LSW7GV2Q.odawara.point.app',
+            paths: ['*'],
+          },
+          {
+            appID: '678U9GD354.com.odawara.dev',
+            paths: ['*'],
+          },
+        ],
+      },
+      webcredentials: {
+        apps: ['Y7LSW7GV2Q.odawara.point.app'],
+      },
+    };
+
+    return body;
   }
 }
